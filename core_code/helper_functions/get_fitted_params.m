2e943c1cef5a86baee89fd130d744e0437a5271f
Ari Khoudary <makhouda@uci.edu>
2026-03-27 18:44:31 -0700

    add code for plotting coefficients over time

--- core_code/helper_functions/get_fitted_params.m
+++ core_code/helper_functions/get_fitted_params.m
@@ -5,12 +5,33 @@ for model = 1:length(models)
     for t = 1:nBins
         coef_names = Fitted_models(t, m).g.CoefficientNames;
         for i = 1:length(coef_names) % columns = coefficient
-            betas(t, i) = Fitted_models(t, m).g.Coefficients.Estimate(i);
-            SEs(t, i) = Fitted_models(t, m).g.Coefficients.SE(i);
-            t_vals(t, i) = Fitted_models(t, m).g.Coefficients.tStat(i);
-            p_vals(t, i) = Fitted_models(t, m).g.Coefficients.pValue(i);
+            fitted_betas(t, i) = Fitted_models(t, m).g.Coefficients.Estimate(i);
+            fitted_SEs(t, i) = Fitted_models(t, m).g.Coefficients.SE(i);
+            fitted_t_vals(t, i) = Fitted_models(t, m).g.Coefficients.tStat(i);
+            fitted_p_vals(t, i) = Fitted_models(t, m).g.Coefficients.pValue(i);
         end
     end
 end
 
-%% to plot error bars, do betas + SEs (upper bound) and betas - SEs (lower bound)
+%% plot beta timecourses from interaction model
+coef_names
+figure;
+hold on
+yline(0, 'HandleVisibility', 'off')
+for i = 1:length(coef_names)
+    errorbar(1:50, fitted_betas(:, i), fitted_SEs(:, i), 'DisplayName', coef_names{i});
+end
+
+xlabel('time')
+ylabel('beta value')
+ylim([-0.5, 0.5])
+xregion([1, 6]);
+xregion([21,41], 'HandleVisibility', 'off');
+xregion([14 20], 'FaceColor', 'r', 'FaceAlpha', 0.1)
+
+legend({'intercept', ...
+    'RT', 'coherence', 'performance', 'accuracy', 'volatility', ...
+    'interaction', ... 
+    'vol alone winning BIC',...
+    'int winning by AIC&BIC'...
+    }, 'Location', 'eastoutside')
