770599ab527ec96d235e9e4bb3fee5a09ec52dc0
yuzhengwu0 <yuzhengwu2329@gmail.com>
2026-03-31 11:51:15 -0700

    add accuracy as regression outcome script (with perf in the interaction term)

---
+++ core_code/helper_functions/fit_model/build_model_family_accuracy.m
@@ -0,0 +1,64 @@
+function [modelNames, modelSpec, baseLabels, oneWayNames, oneWayLabels, ...
+    twoWayNames, twoWayLabels, threeWayNames, threeWayLabels] = build_model_family_do_accuracy()
+
+% model family - accuracy as regressor
+% fixed terms: RT (R),  coherence (coh), vol condition (z_cond)
+% M0: intercept + R + coh + z_cond
+% M1: intercept + R + coh + z_cond + P
+% M2: intercept + R + coh + z_cond + V
+% M3: intercept + R + coh + z_cond + P + V
+% M4: intercept + R + coh + z_cond + P + V + P*V
+% M6: intercept + R + coh + z_cond + P + V + P*V*coh
+
+baseLabels    = {'b0 (Intercept)','b_{corr}','b_{rt}','b_{coh}','b_{cond}'};
+
+oneWayNames   = ["P", "V"];
+oneWayLabels  = {'b_{perf}','b_{vol}'};
+
+twoWayNames   = ["PxV", "CxV"];
+twoWayLabels  = {'b_{perf×vol}', 'b_{corrxvol}'};
+
+threeWayNames   = ["PxVxcoh", 'CxVxcoh'];
+threeWayLabels  = {'b_{perf×volxcoh}', 'b_{corrxvolxcoh}'};
+
+% each row:
+% [name, use1, use2, use3]
+defs = {
+    'M0_base',       [],      [],     false;
+    'M1_P',           1,      [],     false;
+    'M2_V',           2,      [],     false;
+    'M3_P+V',       1:2,      [],     false;
+    'M4_PxV',       1:2,      1,      false;
+    'M6_PxVxcoh',   1:2,      1,       1;
+};
+
+nModels    = size(defs, 1);
+modelNames = defs(:,1)';
+
+modelSpec = struct( ...
+    'use1', cell(1,nModels), ...
+    'use2', cell(1,nModels), ...
+    'use3', cell(1,nModels));
+
+for i = 1:nModels
+    u1 = false(1, numel(oneWayNames));
+    if ~isempty(defs{i,2})
+        u1(defs{i,2}) = true;
+    end
+
+    u2 = false(1, numel(twoWayNames));
+    if ~isempty(defs{i,3})
+        u2(defs{i,3}) = true;
+    end
+
+    u3 = false(1, numel(threeWayNames));
+    if ~isempty(defs{i,4})
+        u3(defs{i,4}) = true;
+    end
+
+    modelSpec(i).use1 = u1;
+    modelSpec(i).use2 = u2;
+    modelSpec(i).use3 = u3;
+end
+
+end
