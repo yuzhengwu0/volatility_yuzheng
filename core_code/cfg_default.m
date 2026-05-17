67d51c249a5ddf4ac577253f05d214e826b35c04
yuzhengwu0 <yuzhengwu2329@gmail.com>
2026-05-16 20:01:53 -0700

    add adjustable nBins for RTtask, and fix nBins == 38 in notRTtask

--- core_code/cfg_default.m
+++ core_code/cfg_default.m
@@ -1,22 +1,26 @@
 % ===== situation =====
-cfg.CORR             = 'all';        % 'all' / 'corr' / 'incorr'
+cfg.CORR             = 'incorr';        % 'all' / 'corr' / 'incorr'
 % cfg.COHHH            = '512';      % '0', '32', '64', '128', '256', '512' / 'all'
 cfg.DROP_HIGHEST_COH = false;         % remove coh == 512
 cfg.REDEFINE_ANSWER  = false;    % true = correct >= ME mean
-cfg.FLIPPING         = false;
+cfg.FLIPPING         = true;
 cfg.HIGHVOL          = false;
 cfg.VOLMODE          = 'vol3abs';   % 'vol1' - difference from trial_coh; 'vol2' - difference of last frame
                                  % 'vol3dir' - cumulative with direction;
                                  % 'vol3abs' - cumulative with abs val
-cfg.RTtask           = false;
+cfg.RTtask           = true;
 
 
 % ===== predictor =====
-cfg.P_PERF_MODE      = 'online';     % 'all' / 'online'
-cfg.nBins            = 27;
+cfg.nBins            = 35;
+if cfg.RTtask == false
+    cfg.nBins            = 38;
+end 
 cfg.winLen           = 5;
 cfg.tol              = 1e-12;
 cfg.useSubjDummies   = false;
+cfg.P_PERF_MODE      = 'online';     % 'all' / 'online'
+
 
 % ===== model =====
 cfg.MODEL_FAMILY     = 'wyz';      % 'cond' / 'coh' / 'blend'/ 'wyz' / 'wyzcoh'
