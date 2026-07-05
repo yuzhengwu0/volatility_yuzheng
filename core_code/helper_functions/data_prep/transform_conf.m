cc2198dbd9ae48a50a0d17cbe1fd8f8e167a9a23
yuzhengwu0 <yuzhengwu2329@gmail.com>
2026-07-05 13:00:34 +0800

    revise the format

--- core_code/helper_functions/data_prep/transform_conf.m
+++ core_code/helper_functions/data_prep/transform_conf.m
@@ -9,34 +9,34 @@ function [ConfY, confCont] = transform_conf(cfg)
 confCont = cfg.confCont;
 subjID = cfg.subjID;
 
-    ConfY = nan(size(confCont));
+ConfY = nan(size(confCont));
 
-    for i = 1:length(confCont)
-        if confCont(i) > 1
-            confCont(i) = 1;
-        elseif confCont(i) < 0
-            confCont(i) = 0;
-        else
-            confCont(i) = confCont(i);
-        end
+for i = 1:length(confCont)
+    if confCont(i) > 1
+        confCont(i) = 1;
+    elseif confCont(i) < 0
+        confCont(i) = 0;
+    else
+        confCont(i) = confCont(i);
     end
+end
 
-    subj_list = unique(subjID);
-    nSubj = numel(subj_list);
+subj_list = unique(subjID);
+nSubj = numel(subj_list);
 
-    for iSub = 1:nSubj
-        s = subj_list(iSub);
-        idxS = subjID == s;
+for iSub = 1:nSubj
+    s = subj_list(iSub);
+    idxS = subjID == s;
 
-        y = confCont(idxS);
+    y = confCont(idxS);
 
-        mu = mean(y, 'omitnan');
-        sigma = std(y, 'omitnan');
+    mu = mean(y, 'omitnan');
+    sigma = std(y, 'omitnan');
 
-        if sigma == 0 || isnan(sigma)
-            ConfY(idxS) = zeros(size(y));
-        else
-            ConfY(idxS) = (y - mu) ./ sigma;
-        end
+    if sigma == 0 || isnan(sigma)
+        ConfY(idxS) = zeros(size(y));
+    else
+        ConfY(idxS) = (y - mu) ./ sigma;
     end
+end
 end
